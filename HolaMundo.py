e597385754e6063bacf8ec86776aff8c1a8c3133
Tavial <correo@tavial.es>
2020-10-25 15:33:46 +0100

    Saludamos a España Segunda modificación

--- HolaMundo.py
+++ HolaMundo.py
@@ -3,4 +3,10 @@ Created on 25 oct. 2020
 
 @author: tavia
 '''
+
+from Paises import Espanya
+
 print ("Hola mundo!!!")
+
+spain = Espanya.Espanya ()
+print (spain.saludoAEspanya())
---
+++ Paises/Espanya.py
@@ -0,0 +1,8 @@
+'''
+Created on 25 oct. 2020
+
+@author: tavia
+'''
+class Espanya:
+    def saludoAEspanya(self):
+        return "Hola España"
