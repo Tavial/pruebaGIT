2aebb394715772cdb86e55b75bc3a2b4cb401d54
Tavial <correo@tavial.es>
2020-10-25 20:58:08 +0100

    Saludamos a Nueva Zelanda Tercera modificación en la feature Tatiana

--- HolaMundo.py
+++ HolaMundo.py
@@ -4,9 +4,11 @@ Created on 25 oct. 2020
 @author: tavia
 '''
 
-from Paises import Espanya
+from Paises import Espanya, NuevaZelanda
 
 print ("Hola mundo!!!")
 
 spain = Espanya.Espanya ()
+nz = NuevaZelanda.NuevaZelanda ()
 print (spain.saludoAEspanya())
+print (nz.saludoANuevaZelanda())
